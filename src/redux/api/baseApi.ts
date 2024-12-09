d6fe84adf65348115d04f7593241858c4464da49
Mirhasankhan <mirhasan000034@gmail.com>
2024-12-09 14:49:23 +0600

    api updated

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl:
+      "https://l2-b2-frontend-path-assignment-6-server-starter-pack-one.vercel.app/api/v1",
   }),
   tagTypes: ["supply"],
   endpoints: () => ({}),
